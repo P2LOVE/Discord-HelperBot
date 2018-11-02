784395f480fb67e7b26755c4dd2232424ebfdc18
LoliPain <p2love.loli@gmail.com>
2018-11-02 07:02:48 +0400

    fixes

--- HelperScript.py
+++ HelperScript.py
@@ -3,41 +3,74 @@ from discord.ext import commands
 import json, urllib.request, socket, sys
 Server = "irc.ppy.sh"
 Port = 6667
-token = open('key.txt')
+token = open('key.txt') # 1st Line -  IRC Username; 2nd - IRC Password; 3rd - Discord Bot Token
 Key = token.readlines()
 Username = Key[0]
 ServerPassword = Key[1]
 chillrdy = int('1')
 bot = commands.Bot(command_prefix='~', description='Helping us help you help us all')
-
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc.connect((Server, Port))
-data = irc.recv(1024).decode()
 print('---------------')
-print(data)
-irc.send(data.replace('ping', 'pong').encode())
-irc.send('USER'.encode())
-print(irc.recv(1024).decode())
-irc.send(Username.encode())
-print(irc.recv(1024).decode())
-irc.send('PASS'.encode())
-print(irc.recv(1024).decode())
-irc.send(ServerPassword.encode())
-print(irc.recv(1024).decode())
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Set IRC socket
+irc.connect((Server, Port))
+irc.send(('PASS ' + ServerPassword + '\r\n').encode()) # Read Password from key.txt
+irc.send(('NICK ' + Username + '\r\n').encode()) # Same 
+irc.send(('END \r\n').encode())
 print("Connected to: BanchoIRC")
-
+data = irc.recv(2048).decode() # Welcome message
+print(data)
 
 @bot.event
 async def on_ready():
+    print('---------------')
     print('Welcome back!')
     print(bot.user.name)
     print(bot.user.id)
     print('---------------')
-@bot.command()
-async def adioni(ctx):
-    irc.send('msg adioni 123'.encode())
-    irc.send('msg adioni: 123'.encode())
-    
+@bot.command(pass_context=True)
+async def check(ctx):
+        Msg = ctx.message.content
+        #debug print(Msg)
+        if Msg.find('check') > 0 :
+            Player = Msg[7:] # Argument after ~check
+           #debug print(Player)
+            irc.send('PRIVMSG BanchoBot stats '.encode() + Player.encode() + ' \r\n'.encode())
+            Check = 0
+            while Check < 5:
+                Recive = irc.recv(100).decode()
+                OnlineSt = Recive.find('Idle:')
+                PlaySt = Recive.find('Playing:')
+                MapSt = Recive.find('Editing:')
+                ModSt = Recive.find('Modding:')
+                TestSt = Recive.find('Testing:')
+                AfkSt = Recive.find('Afk:')
+                if OnlineSt > 0:
+                    Check = 5
+                    await ctx.send(Player + ' just Online!')
+                else:
+                    if PlaySt > 0:
+                        Check = 5
+                        await ctx.send(Player + ' just Playing!')
+                    else:
+                        if MapSt > 0:
+                            Check = 5
+                            await ctx.send(Player + ' just Editing!')
+                        else:
+                            if ModSt > 0:
+                                Check = 5
+                                await ctx.send(Player + ' just Modding!')
+                            else:
+                                if TestSt > 0:
+                                    Check = 5
+                                    await ctx.send(Player + ' just Testing the map!')
+                                else:
+                                    if AfkSt > 0:
+                                        Check = 5
+                                        await ctx.send(Player + ' just AFK!')
+                                    else:
+                                        Check = Check + 0.1 # 50 cycle retries
+                                        print(Check)
+                                        if Check > 5 | (TestSt == 0 & ModSt == 0 & MapSt == 0 & TestSt == 0 & OnlineSt == 0 & PlaySt == 0):
+                                            await ctx.send(Player + ' Offline :(')
 @bot.command()
 async def chill(ctx):
     chillrdy = '1'
@@ -49,14 +82,14 @@ async def chill(ctx):
     embed.add_field(name="H", value="1 Hour Chill Mix")
 
     embed.add_field(name="I", value="2 Hour Chill Mix")
-	
+    
     embed.add_field(name="L", value="Rameses B Chill Mix")
                     
     embed.add_field(name="LL", value="Random Mix")
 
     await ctx.send(embed=embed)
 
-    while chillrdy == '1':
+    if chillrdy == '1':
         @bot.command()
         async def C(ctx):
             chillrdy = '0'
