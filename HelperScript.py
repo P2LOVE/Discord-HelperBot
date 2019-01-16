6335a532f984de1ac6f160416f6fd5bbbaa1b9a6
P2LOVE <40074412+P2LOVE@users.noreply.github.com>
2019-01-17 03:16:01 +0400

    some improvements

--- HelperScript.py
+++ HelperScript.py
@@ -5,8 +5,11 @@ from asyncio import sleep
 
 Server = "irc.ppy.sh"
 Port = 6667
+roles_txt = open('roles.txt') # 1 line = 1 role
+list_of_roles = roles_txt.readlines()
 token = open('key.txt') # 1st Line -  IRC Username; 2nd - IRC Password; 3rd - Discord Bot Token
 Key = token.readlines()
+mycreator = '266419230587617292'
 Username = Key[0]
 ServerPassword = Key[1]
 Recive = None
@@ -50,5 +53,13 @@ async def on_message(message):
         if (Player == 'MyHeroMismagius') or (Player == 'p2love'):
             await bot.send_message(message.channel, 'My creator always online in my heart <3')
     
+    if message.content == 'authorize':
+        author = message.author
+        authorid = message.author.id
+        if (authorid == mycreator):
+            server = bot.get_server(message.server)
+            role = discord.utils.get(server.roles, name=list_of_roles[0]) 
+            await bot.add_roles(author, role)
+            await bot.send_message(message.channel, 'Welcome back, senpai ♥')
 
 bot.run(Key[2])
